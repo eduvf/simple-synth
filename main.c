58a91361f8cca3755651afd46d93417ce0aa2dd1
EduVF <eduardvf@outlook.com>
2023-08-09 13:15:02 +0200

    add synth slider

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 // https://www.youtube.com/watch?v=fqUbYIhLTqw
-// 14:00
 
 #include <math.h>
 #include <stddef.h>
@@ -36,6 +35,7 @@ typedef struct
     float *signal;
     size_t signal_length;
     float audio_frame_duration;
+    float base_freq;
 } Synth;
 
 typedef float (*WaveShapeFn)(Oscillator *);
@@ -100,8 +100,9 @@ float sqrWaveOsc(Oscillator *osc)
 }
 
 void updateOscArray(WaveShapeFn base_osc_shape_fn, WaveShapeFn lfo_osc_shape_fn,
-                    Synth *synth, Oscillator *osc_array, float freq)
+                    Synth *synth, Oscillator *osc_array)
 {
+    const float freq = synth->base_freq;
     for (size_t i = 0; i < synth->n_oscillators; i++)
     {
         osc_array[i].freq = freq * (i + 1);
@@ -121,25 +122,21 @@ void updateOscArray(WaveShapeFn base_osc_shape_fn, WaveShapeFn lfo_osc_shape_fn,
 
 void handleAudioStream(AudioStream stream, Synth *synth)
 {
-    Vector2 mouse_pos = GetMousePosition();
-    float normalized_mouse_x = (mouse_pos.x / SCREEN_WIDTH);
-    float normalized_mouse_y = (mouse_pos.y / SCREEN_HEIGHT);
-    float base_freq = 50.0f + (normalized_mouse_x * 500.0f);
-    synth->lfo.freq = 0.05f + (normalized_mouse_y * 10.0f);
+    // Vector2 mouse_pos = GetMousePosition();
+    // float normalized_mouse_x = (mouse_pos.x / SCREEN_WIDTH);
+    // float normalized_mouse_y = (mouse_pos.y / SCREEN_HEIGHT);
+    // synth->base_freq = 50.0f + (normalized_mouse_x * 500.0f);
+    // synth->lfo.freq = 0.05f + (normalized_mouse_y * 10.0f);
     float audio_frame_duration = 0.0f;
 
     if (IsAudioStreamProcessed(stream))
     {
         const float audio_frame_start_time = GetTime();
         zeroSignal(synth->signal);
-        updateOscArray(&sinWaveOsc, &sinWaveOsc, synth, synth->sinOsc,
-                       base_freq);
-        updateOscArray(&sawWaveOsc, &sinWaveOsc, synth, synth->sawOsc,
-                       base_freq);
-        updateOscArray(&triWaveOsc, &sinWaveOsc, synth, synth->triOsc,
-                       base_freq);
-        updateOscArray(&sqrWaveOsc, &sinWaveOsc, synth, synth->sqrOsc,
-                       base_freq);
+        updateOscArray(&sinWaveOsc, &sinWaveOsc, synth, synth->sinOsc);
+        updateOscArray(&sawWaveOsc, &sinWaveOsc, synth, synth->sawOsc);
+        updateOscArray(&triWaveOsc, &sinWaveOsc, synth, synth->triOsc);
+        updateOscArray(&sqrWaveOsc, &sinWaveOsc, synth, synth->sqrOsc);
         UpdateAudioStream(stream, synth->signal, synth->signal_length);
         synth->audio_frame_duration = GetTime() - audio_frame_start_time;
     }
@@ -166,10 +163,9 @@ int main()
 
     GuiLoadStyle("./cyber/cyber.rgs");
 
-    unsigned int sample_rate = SAMPLE_RATE;
     SetAudioStreamBufferSizeDefault(STREAM_BUFFER_SIZE);
     AudioStream synth_stream =
-        LoadAudioStream(sample_rate, sizeof(float) * 8, 1);
+        LoadAudioStream(SAMPLE_RATE, sizeof(float) * 8, 1);
     SetAudioStreamVolume(synth_stream, 0.05f);
     PlayAudioStream(synth_stream);
 
@@ -186,7 +182,8 @@ int main()
                    .lfo = {.phase = 0.0f, .amp = 0.0f},
                    .signal = signal,
                    .signal_length = STREAM_BUFFER_SIZE,
-                   .audio_frame_duration = 0.0f};
+                   .audio_frame_duration = 0.0f,
+                   .base_freq = 440.0f};
 
     for (size_t i = 0; i < NUM_OSCILLATORS; i++)
     {
@@ -198,6 +195,7 @@ int main()
     }
 
     const float panel_width = width / 4.0f;
+    const int inner_panel_width = panel_width - 20;
 
     while (!WindowShouldClose())
     {
@@ -206,39 +204,54 @@ int main()
         BeginDrawing();
         ClearBackground(BLACK);
 
+        // GUI
         GuiGrid((Rectangle){0, 0, width, height}, "hi", height / 10.0f, 2, 0);
-        GuiPanel((Rectangle){0, 0, panel_width, height}, "Panel");
+        GuiPanel((Rectangle){0, 0, panel_width, height}, NULL);
 
+        int add_osc =
+            GuiButton((Rectangle){10, 10, inner_panel_width, 20}, "Add osc");
+        if (add_osc)
         {
-            size_t zero_crossing_idx = 0;
-            for (size_t i = 1; i < STREAM_BUFFER_SIZE; i++)
-            {
-                if (signal[i] >= 0.0f && signal[i - 1] < 0.0f)
-                {
-                    zero_crossing_idx = i;
-                    break;
-                }
-            }
-
-            Vector2 signal_points[STREAM_BUFFER_SIZE];
-            const float screen_vert_midpoint = (float)(SCREEN_HEIGHT) / 2;
-            for (size_t point_idx = 0; point_idx < STREAM_BUFFER_SIZE;
-                 point_idx++)
-            {
-                const size_t signal_idx =
-                    (point_idx + zero_crossing_idx) % STREAM_BUFFER_SIZE;
-                signal_points[point_idx].x = (float)point_idx + panel_width;
-                signal_points[point_idx].y =
-                    screen_vert_midpoint + (int)(signal[signal_idx] * 100);
-            }
-            // DrawLine(0, screen_vert_midpoint, SCREEN_WIDTH,
-            //          screen_vert_midpoint, DARKGRAY);
-            DrawLineStrip(signal_points, STREAM_BUFFER_SIZE - zero_crossing_idx,
-                          YELLOW);
-
-            DrawText(TextFormat("Zero crossing index: %i", zero_crossing_idx),
-                     panel_width + 10, 30, 20, RED);
+            printf("Add synth\n");
         }
+
+        GuiPanel((Rectangle){10, 40, inner_panel_width, 60}, NULL);
+
+        float log_freq = log10f(synth.base_freq);
+        GuiLabel((Rectangle){20, 40, inner_panel_width - 20, 20},
+                 TextFormat("Freq: %f", synth.base_freq));
+        GuiSlider((Rectangle){20, 60, inner_panel_width - 20, 20}, NULL, NULL,
+                  &log_freq, 0.0f, log10f((float)(SAMPLE_RATE / 2.0f)));
+        synth.base_freq = powf(10.0f, log_freq);
+
+        // Draw signal
+        size_t zero_crossing_idx = 0;
+        for (size_t i = 1; i < STREAM_BUFFER_SIZE; i++)
+        {
+            if (signal[i] >= 0.0f && signal[i - 1] < 0.0f)
+            {
+                zero_crossing_idx = i;
+                break;
+            }
+        }
+
+        Vector2 signal_points[STREAM_BUFFER_SIZE];
+        const float screen_vert_midpoint = (float)(SCREEN_HEIGHT) / 2;
+        for (size_t point_idx = 0; point_idx < STREAM_BUFFER_SIZE; point_idx++)
+        {
+            const size_t signal_idx =
+                (point_idx + zero_crossing_idx) % STREAM_BUFFER_SIZE;
+            signal_points[point_idx].x = (float)point_idx + panel_width;
+            signal_points[point_idx].y =
+                screen_vert_midpoint + (int)(signal[signal_idx] * 100);
+        }
+        // DrawLine(0, screen_vert_midpoint, SCREEN_WIDTH,
+        //          screen_vert_midpoint, DARKGRAY);
+        DrawLineStrip(signal_points, STREAM_BUFFER_SIZE - zero_crossing_idx,
+                      YELLOW);
+
+        DrawText(TextFormat("Zero crossing index: %i", zero_crossing_idx),
+                 panel_width + 10, 30, 20, RED);
 
         DrawText(TextFormat("FPS: %i, delta: %f", GetFPS(), GetFrameTime()),
                  panel_width + 10, 50, 16, RED);
